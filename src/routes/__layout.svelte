aa01951e3a4046dd409cdc9c079ee59d0a63bed8
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2022-07-01 00:03:58 +0200

    CSS...

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,14 @@
 
 <body>
     <Nav />
-    <main class="container">
+    <main class="container workArea">
         <slot />
     </main>
 </body>
+
+<style>
+    .workArea {
+        width: 80%;
+        max-width: 90%;
+    }
+</style>
